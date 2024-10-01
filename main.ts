83e46cfaa8b8bb7733bf9a34a25a4f30625fd816
Evan-Maddicks <evan.maddicks@mths.ca>
2024-10-01 11:20:22 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Evan M
+ * Created on: Sep 2024
+ * This program will allow you to "click cookies"
 */
 
-basic.showString('Hello, World!')
+let CookieNumber = 0
+
+input.onButtonPressed(Button.A,function () {
+CookieNumber += 1
+basic.showNumber(CookieNumber)
+})
